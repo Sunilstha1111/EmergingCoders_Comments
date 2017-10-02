2b4a07cfeeb7b17338470198d8bf3b5b4f1120ac
Sunil Shrestha <sunilstha108@gmail.com>
2017-10-02 17:08:50 -0400

    Initial Commit

---
+++ src/EmergingCoders_Comments.java
@@ -0,0 +1,12 @@
+public class EmergingCoders_Comments {
+    public static void main (String[] args){
+
+        //Age = 21
+        //Name: Sunil Shrestha
+        int x = 21;
+        double y = 21;
+        double Sum = x + y;
+
+        System.out.println("Sum:" + Sum);
+    }
+}
